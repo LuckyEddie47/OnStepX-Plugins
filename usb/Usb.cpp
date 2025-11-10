32b6f2e8f37579cddf90d1383c41a1eae35adc6d
Ed Lee <ed@thefamilee.co.uk>
2025-11-10 19:41:48 +0000

    Now compiling

--- usb/Usb.cpp
+++ usb/Usb.cpp
@@ -2,8 +2,20 @@
 
 #include "Usb.h"
 //#include "../../lib/tasks/OnTask.h"
+#include "../../lib/gpioEx/GpioEx.h"
 
 //void usbWrapper() { usb.loop(); }
+
+Port myPort[8] = {
+      { USB1_NAME, USB1_PIN, USB1_DEFAULT_STATE, USB1_ON_STATE },
+      { USB2_NAME, USB2_PIN, USB2_DEFAULT_STATE, USB2_ON_STATE },
+      { USB3_NAME, USB3_PIN, USB3_DEFAULT_STATE, USB3_ON_STATE },
+      { USB4_NAME, USB4_PIN, USB4_DEFAULT_STATE, USB4_ON_STATE },
+      { USB5_NAME, USB5_PIN, USB5_DEFAULT_STATE, USB5_ON_STATE },
+      { USB6_NAME, USB6_PIN, USB6_DEFAULT_STATE, USB6_ON_STATE },
+      { USB7_NAME, USB7_PIN, USB7_DEFAULT_STATE, USB7_ON_STATE },
+      { USB8_NAME, USB8_PIN, USB8_DEFAULT_STATE, USB8_ON_STATE }
+    };
 
 void Usb::init() {
   VLF("MSG: Plugins, starting: USB port switcher");
@@ -42,7 +54,7 @@ void Usb::init() {
 //void Usb::loop() {}
 
 // Command process is a copy and paste from Features.command.cpp with unwanted stripped out and names changed
-bool Sample::command(char *reply, char *command, char *parameter, bool *supressFrame, bool *numericReply, CommandError *commandError) {
+bool Usb::command(char *reply, char *command, char *parameter, bool *supressFrame, bool *numericReply, CommandError *commandError) {
   
   // Get USB commands
   if (command[0] == 'G' && command[1] == 'U' && parameter[2] == 0) {
@@ -52,7 +64,7 @@ bool Sample::command(char *reply, char *command, char *parameter, bool *supressF
       int i = parameter[1] - '1';
       if (i < 0 || i > 7)  { *commandError = CE_PARAM_FORM; return true; }
       char s[255];
-        sprintf(s, "%d", device[i].value);
+        sprintf(s, "%d", myPort[i].value);
         strcat(reply, s);
     } else 
       
@@ -62,7 +74,7 @@ bool Sample::command(char *reply, char *command, char *parameter, bool *supressF
       int i = parameter[1] - '1';
       if (i == -1) {
         for (int j = 0; j < 8; j++) {
-          if (port[j].pin == OFF) reply[j] = '0'; else reply[j] = '1';
+          if (myPort[j].pin == OFF) reply[j] = '0'; else reply[j] = '1';
           reply[j + 1] = 0;
         }
         *numericReply = false;
@@ -72,9 +84,9 @@ bool Sample::command(char *reply, char *command, char *parameter, bool *supressF
       // :GUY[n]#
       // where [n] = 1..8 to get USB port name
       if (i < 0 || i > 7)  { *commandError = CE_PARAM_FORM; return true; }
-      if (port[i].pin == OFF) { *commandError = CE_0; return true; }
+      if (myPort[i].pin == OFF) { *commandError = CE_0; return true; }
       char s[255];
-      strcpy(s, port[i].name);
+      strcpy(s, myPort[i].name);
       if (strlen(s) > 10) s[10] = 0;
       strcpy(reply, s);
 
@@ -89,7 +101,7 @@ bool Sample::command(char *reply, char *command, char *parameter, bool *supressF
     if (parameter[0] == 'X') { 
       int i = parameter[1] - '1';
       if (i < 0 || i > 7)  { *commandError = CE_PARAM_FORM; return true; }
-      if (port[i].pin == OFF) { *commandError = CE_CMD_UNKNOWN; return true; }
+      if (myPort[i].pin == OFF) { *commandError = CE_CMD_UNKNOWN; return true; }
 
       char* conv_end;
       float f = strtof(&parameter[4], &conv_end);
@@ -98,7 +110,7 @@ bool Sample::command(char *reply, char *command, char *parameter, bool *supressF
 
       if (parameter[3] == 'V') {
         if (v >= 0 && v <= 1) { // value 0..1 for enabled or not
-          digitalWriteEx(port[i].pin, v == port[i].active);
+          digitalWriteEx(myPort[i].pin, v == myPort[i].active);
         } else *commandError = CE_PARAM_RANGE;
       } else *commandError = CE_PARAM_FORM;
     } else return false;
